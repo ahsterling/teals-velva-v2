08890a2eeae17072dfd06b09424ca595bc09b01e
Allie Sterling <allies@zillowgroup.com>
2019-05-02 23:12:41 -0700

    adds index.html

--- app.py
+++ app.py
@@ -1,9 +1,58 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+@app.route('/adam')
+def adam():
+    return render_template('students/adam.html')
+
+@app.route('/wyatt')
+def wyatt():
+    return render_template('students/wyatt.html')
+
+@app.route('/paul')
+def paul():
+    return render_template('students/paul.html')
+
+@app.route('/grace')
+def grace():
+    return render_template('students/grace.html')
+
+@app.route('/isabella')
+def isabella():
+    return render_template('students/isabella.html')
+
+@app.route('/daniel')
+def daniel():
+    return render_template('students/daniel.html')
+
+@app.route('/morgan')
+def morgan():
+    return render_template('students/morgan.html')
+
+@app.route('/gunnar')
+def gunnar():
+    return render_template('students/gunnar.html')
+
+@app.route('/bryson')
+def bryson():
+    return render_template('students/bryson.html')
+
+@app.route('/jerry')
+def jerry():
+    return render_template('students/jerry.html')
+
+@app.route('/michael')
+def michael():
+    return render_template('students/michael.html')
+
+@app.route('/skyler')
+def skyler():
+    return render_template('students/skyler.html')
 
 if __name__ == '__main__':
     app.run()
