f15aa16f68f04f4dbfa9ed9dd2590c4995a405d5
Allie Sterling <allies@zillowgroup.com>
2019-05-04 23:28:12 -0700

    update student templates

--- app.py
+++ app.py
@@ -8,51 +8,51 @@ def index():
 
 @app.route('/adam')
 def adam():
-    return render_template('students/adam.html', name="Adam")
+    return render_template('students/adam.html', name="Adam", grad_year="?")
 
 @app.route('/wyatt')
 def wyatt():
-    return render_template('students/wyatt.html', name="Wyatt")
+    return render_template('students/wyatt.html', name="Wyatt", grad_year="?")
 
 @app.route('/paul')
 def paul():
-    return render_template('students/paul.html', name="Paul")
+    return render_template('students/paul.html', name="Paul", grad_year="?")
 
 @app.route('/grace')
 def grace():
-    return render_template('students/grace.html', name="Grace")
+    return render_template('students/grace.html', name="Grace", grad_year="?")
 
 @app.route('/isabella')
 def isabella():
-    return render_template('students/isabella.html', name="Isabella")
+    return render_template('students/isabella.html', name="Isabella", grad_year="?")
 
 @app.route('/daniel')
 def daniel():
-    return render_template('students/daniel.html', name="Daniel")
+    return render_template('students/daniel.html', name="Daniel", grad_year="?")
 
 @app.route('/morgan')
 def morgan():
-    return render_template('students/morgan.html', name="Morgan")
+    return render_template('students/morgan.html', name="Morgan", grad_year="?")
 
 @app.route('/gunnar')
 def gunnar():
-    return render_template('students/gunnar.html', name="Gunnar")
+    return render_template('students/gunnar.html', name="Gunnar", grad_year="?")
 
 @app.route('/bryson')
 def bryson():
-    return render_template('students/bryson.html', name="Bryson")
+    return render_template('students/bryson.html', name="Bryson", grad_year="?")
 
 @app.route('/jerry')
 def jerry():
-    return render_template('students/jerry.html', name="Jerry")
+    return render_template('students/jerry.html', name="Jerry", grad_year="?")
 
 @app.route('/michael')
 def michael():
-    return render_template('students/michael.html', name="Michael")
+    return render_template('students/michael.html', name="Michael", grad_year="?")
 
 @app.route('/skyler')
 def skyler():
-    return render_template('students/skyler.html', name="Skyler")
+    return render_template('students/skyler.html', name="Skyler", grad_year="?")
 
 if __name__ == '__main__':
     app.run()
